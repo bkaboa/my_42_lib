338efb8f362eaac7768c0a944056184d8ea0bd21
bkaboa <73066722+bkaboa@users.noreply.github.com>
2026-01-18 13:25:02 +0100

    [MODIFY] colors in logger

--- logger/ft_color.h
+++ logger/ft_color.h
@@ -1,6 +1,9 @@
 #ifndef COLOR_H
 # define COLOR_H
 
+#define COLOR_TEXT_LENGTH 5
+#define RESET_COLOR_TEXT_LENGTH 4
+
 #define COLOR_RESET "\033[0m"
 #define COLOR_RED "\033[31m"
 #define COLOR_GREEN "\033[32m"
